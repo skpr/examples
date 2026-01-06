621f09d70b613543de30ab3080caaebc48df66e7
Nathan ter Bogt <nterbogt@gmail.com>
2026-01-07 12:48:15 +1300

    Updating Drupal example to match current standards (#2)
    * Updating to latest version of things
    
    * Refactoring repo to match how we would really set it up. Fixing issues with docker-compose. Updating images and username/password pairs
    
    * Final updates
    
    * Adding workflow for deploying drupal automatically
    
    * Sending up a build
    
    * Updating based on working directory
    
    * Around again
    
    * Around again
    
    * Don't forget to package
    
    * Updating to use the right file paths
    
    * Make deploy do an update instead
    
    * Use quotes for passing command
    
    * Only update Drupal when Drupal changes on main
    
    * Use 'mail' as the name in docker

--- drupal/app/sites/default/settings.php
+++ drupal/app/sites/default/settings.php
@@ -13,24 +13,26 @@ $settings['allow_authorize_operations'] = FALSE;
 $databases['default']['default'] = array(
   'driver' => 'mysql',
   'database' => $skpr->get('mysql.default.database') ?: 'local',
-  'username' => $skpr->get('mysql.default.username') ?: 'drupal',
-  'password' => $skpr->get('mysql.default.password') ?: 'drupal',
+  'username' => $skpr->get('mysql.default.username') ?: 'local',
+  'password' => $skpr->get('mysql.default.password') ?: 'local',
   'host' => $skpr->get('mysql.default.proxy') ?: '127.0.0.1',
 );
 
+$databases['default']['default']['pdo'][PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT] = false;
+
 $config['cron_safe_threshold'] = '0';
 $settings['file_public_path'] = 'sites/default/files';
-$config['system.file']['path']['temporary'] = '/mnt/temporary';
-$settings['file_private_path'] = '/mnt/private';
+$config['system.file']['path']['temporary'] = $skpr->get('mount.temporary', '/tmp');;
+$settings['file_private_path'] = $skpr->get('mount.private', '/data/app/sites/default/files/private');;
 
-$settings['hash_salt'] = !empty($settings['hash_salt']) ? $settings['hash_salt'] : 'xxxxxxxxxxxxxxxxxxxx';
+$settings['hash_salt'] = $skpr->get('drupal.hash_salt', 'xxxxxxxxxxxxxxxxxxxxxxx');
 
 $settings['trusted_host_patterns'][] = '^127\.0\.0\.1$';
 foreach ($skpr->hostNames() as $hostname) {
   $settings['trusted_host_patterns'][] = '^' . preg_quote($hostname) . '$';
 }
 
-$config['prometheus_exporter_token_access.settings']['access_token'] = $skpr->get('skpr.token') ?: '';
+$config['prometheus_exporter_token_access.settings']['access_token'] = $skpr->get('skpr.token', '');
 
 $config['elasticsearch_connector.cluster.default']['url'] = $skpr->get('elasticsearch.writer.endpoint', 'https://localhost:9200');
 $config['elasticsearch_connector.cluster.default']['options']['username'] = $skpr->get('elasticsearch.writer.username', 'admin');
